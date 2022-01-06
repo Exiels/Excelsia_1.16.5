509f6ca4b9e02fb1ae5d7124b70d9b0ac0867e08
Exiels <Samina29@outlook.fr>
2022-01-06 08:09:55 +0100

    ADD: Full Pannel

---
+++ src/main/java/fr/exiel/excelsia/ui/PanelManager.java
@@ -0,0 +1,86 @@
+package fr.exiel.excelsia.ui;
+
+import com.goxr3plus.fxborderlessscene.borderless.BorderlessScene;
+import fr.exiel.excelsia.Launcher;
+import fr.exiel.excelsia.ui.panel.IPanel;
+import fr.exiel.excelsia.ui.panels.partials.TopBar;
+import fr.flowarg.flowcompat.Platform;
+import javafx.geometry.VPos;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class PanelManager {
+    private final Launcher launcher;
+    private final Stage stage;
+    private GridPane layout;
+    private final TopBar topBar = new TopBar();
+    private final GridPane contentPane = new GridPane();
+
+    public PanelManager(Launcher launcher, Stage stage) {
+        this.launcher = launcher;
+        this.stage = stage;
+    }
+
+    public void init() {
+        this.stage.setTitle("Launcher");
+        this.stage.setWidth(1280);
+        this.stage.setHeight(720);
+        this.stage.centerOnScreen();
+        this.stage.getIcons().add(new Image("images/icon.png"));
+
+        this.layout = new GridPane();
+
+        if (Platform.isOnLinux()) {
+            Scene scene = new Scene(this.layout);
+            this.stage.setScene(scene);
+        } else {
+            this.stage.initStyle(StageStyle.UNDECORATED);
+
+            TopBar topBar = new TopBar();
+            BorderlessScene scene = new BorderlessScene(this.stage, StageStyle.UNDECORATED, this.layout);
+            scene.setResizable(false);
+            scene.setMoveControl(topBar.getLayout());
+            scene.removeDefaultCSS();
+
+            this.stage.setScene(scene);
+
+            RowConstraints topPaneConstraints = new RowConstraints();
+            topPaneConstraints.setValignment(VPos.TOP);
+            topPaneConstraints.setMinHeight(25);
+            topPaneConstraints.setMaxHeight(25);
+            this.layout.getRowConstraints().addAll(topPaneConstraints, new RowConstraints());
+            this.layout.add(topBar.getLayout(), 0, 0);
+            topBar.init(this);
+        }
+
+        this.layout.add(this.contentPane, 0, 1);
+        GridPane.setVgrow(this.contentPane, Priority.ALWAYS);
+        GridPane.setHgrow(this.contentPane, Priority.ALWAYS);
+
+        this.stage.show();
+    }
+
+    public void showPanel(IPanel panel) {
+        this.contentPane.getChildren().clear();
+        this.contentPane.getChildren().add(panel.getLayout());
+        if (panel.getStylesheetPath() != null) {
+            this.stage.getScene().getStylesheets().clear();
+            this.stage.getScene().getStylesheets().add(panel.getStylesheetPath());
+        }
+        panel.init(this);
+        panel.onShow();
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public Launcher getLauncher() {
+        return launcher;
+    }
+}
---
+++ src/main/java/fr/exiel/excelsia/ui/panel/IPanel.java
@@ -0,0 +1,12 @@
+package fr.exiel.excelsia.ui.panel;
+
+import fr.exiel.excelsia.ui.PanelManager;
+import javafx.scene.layout.GridPane;
+
+public interface IPanel {
+    void init(PanelManager panelManager);
+    GridPane getLayout();
+    void onShow();
+    String getName();
+    String getStylesheetPath();
+}
